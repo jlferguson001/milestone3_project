69c6df761aae4630af976ad74b80854786332a7d
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-10-19 21:27:00 -0500

    established backend and routes

---
+++ backend/base/products.py
@@ -0,0 +1,100 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Biohazard Pyrex ',
+        'image': '/images/biohazardpyrex.jpg',
+        'description': 'etched pyrex dish 8 x 8 with lid',
+        'brand': 'pyrex',
+        'category': 'dinnerware', 
+        'price': 25.00,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '2',
+        'name': 'Biohazard Wine Glass',
+        'image': '/images/biohazardwine.jpg',
+        'description': 'etched wine glass',
+        'brand': 'Libby',
+        'category': 'Beverage Container', 
+        'price': 14.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '3',
+        'name': 'Fyling Under the Influence',
+        'image': '/images/flyingUnderinfluence.jpg ',
+        'description': 'etched wine glass',
+        'brand': 'Libby',
+        'category': 'Beverage Container', 
+        'price': 14.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '4',
+        'name': 'Hands Off',
+        'image': '/images/handsoff.jpg ',
+        'description': 'etched pyrex dish 9x13',
+        'brand': 'pyrex',
+        'category': 'dinnerware', 
+        'price': 40.00,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '5',
+        'name': 'Here for the Boos',
+        'image': '/images/hereForTheBoos.jpg ',
+        'description': 'etched wine glass',
+        'brand': 'Libby',
+        'category': 'Beverage Container', 
+        'price': 14.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '6',
+        'name': 'Mr & Mrs wine glasses',
+        'image': '/images/mrandmrswine.jpg ',
+        'description': 'etched wine glass set',
+        'brand': 'Libby',
+        'category': 'Beverage Container', 
+        'price': 50.00,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '7',
+        'name': 'Personalized Pie Pan',
+        'image': '/images/piepan.jpg ',
+        'description': ' ',
+        'brand': 'pyrex',
+        'category': 'dinnerware', 
+        'price': 30.00,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+    {
+        '_id': '8',
+        'name': 'Personalized Pub Glasses',
+        'image': '/images/pubglass.jpg',
+        'description': 'set of 4 personalized pub glasses',
+        'brand': 'Libby',
+        'category': 'Beverage Container', 
+        'price': 50.00,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12 
+    },
+]
+
+ 
