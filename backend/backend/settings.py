b5e504e23fd62cb484b9e17d6aea9bb6b03f5449
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-10-23 11:08:16 -0500

    added superusers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,7 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+#  ['http://localhost:3000']
