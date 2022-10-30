c04ea014e1c5ca80ee7f873ade0a61565b101e25
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-10-29 22:13:18 -0500

    qty select

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listProductDetails } from '../actions/productActions'
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1)
   const match = useParams()
   const dispatch = useDispatch()
   const productDetails = useSelector((state) => state.productDetails)
@@ -71,6 +72,30 @@ function ProductScreen() {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs='auto' className = 'my-1'>
+                        <Form.Control
+                          as = 'select'
+                          value = {qty}
+                          onchange = {(e) => setQty(e.target.value)}
+                        >
+                          {
+                            [...Array(product.countInStock).keys()].map((x) =>(
+                              <option key = {x+1} value = {x+1}>
+                                {x +1}
+                              </option>
+                            ))
+                          }
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                
                 <ListGroup.Item className='d-grid gap-2'>
                   <Button
                     size='lg'
